67eafc12f3e80221fe3bbb1bd320473cdf470871
huzaifa250 <huz.dark1@gmail.com>
2024-05-18 11:12:46 -0700

    edit

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,49 @@
 #!/usr/bin/python3
 """Defines the BaseModel class for all other classes."""
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Represents the BaseModel class for the  project."""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel."""
+        self.id = str(uuid4())  # unique id for each BaseModel
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        time_f = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, time_f)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel obj."""
+        # Get the class name of the instance
+        clname = self.__class__.__name__
+        # Format and return the string representation
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at with the current datetime"""
+        # Update the 'updated_at' attribute with the current date and time
+        self.updated_at = datetime.today()
+        # Save the current state of the obj to persistent storage
+        models.storage.save()
+
+    def to_dict(self):
+        """converts an instance of the BaseModel class into dictionary"""
+        # Create a shallow copy of the instance's dictionary
+        rsdict = self.__dict__.copy()
+        # Convert datetime attributes to ISO format
+        rsdict["created_at"] = self.created_at.isoformat()
+        rsdict["updated_at"] = self.updated_at.isoformat()
+        # Include the class name
+        rsdict["__class__"] = self.__class__.__name__
+        # Return the resulting dictionary
+        return rsdict
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Defines unittests for base_model"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.base_model import BaseModel
+
+
+class TestBaseModel_instantiation(unittest.TestCase):
+    """Testing instantiation of the BaseModel class."""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_id_public_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_created_at_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_updated_at_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
+    def test_models_have_unique_ids(self):
+        base_m1 = BaseModel()
+        base_m2 = BaseModel()
+        self.assertNotEqual(base_m1.id, base_m2.id)
+
+    def test_models_created_at_diffrent_time(self):
+        bm1 = BaseModel()
+        sleep(0.06)
+        bm2 = BaseModel()
+        self.assertLess(bm1.created_at, bm2.created_at)
+
+    def test_models_updated_at_different_time(self):
+        bm1 = BaseModel()
+        sleep(0.06)
+        bm2 = BaseModel()
+        self.assertLess(bm1.updated_at, bm2.updated_at)
+
+    def test_unused_args(self):
+        base_m = BaseModel(None)
+        self.assertNotIn(None, base_m.__dict__.values())
+
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        base_m = BaseModel()
+        base_m.id = "123456"
+        base_m.created_at = base_m.updated_at = dt
+        bmstr = base_m.__str__()
+        self.assertIn("[BaseModel] (123456)", bmstr)
+        self.assertIn("'id': '123456'", bmstr)
+        self.assertIn("'created_at': " + dt_repr, bmstr)
+        self.assertIn("'updated_at': " + dt_repr, bmstr)
+
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            BaseModel(id=None, created_at=None, updated_at=None)
+
+    def test_with_args_and_kwargs(self):
+        dat = datetime.today()
+        dat_iso = dat.isoformat()
+        bm = BaseModel("12", id="345", created_at=dat_iso, updated_at=dat_iso)
+        self.assertEqual(bm.id, "345")
+        self.assertEqual(bm.created_at, dat)
+        self.assertEqual(bm.updated_at, dat)
+
+
+if __name__ == "__main__":
+    unittest.main()
