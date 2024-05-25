7ea19b0814e364c566833c507ad925e848d6b53c
huzaifa250 <huz.dark1@gmail.com>
2024-05-25 16:33:01 -0700

    add web_static

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines unittests for model amenity"""
+
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.amenity import Amenity
+
